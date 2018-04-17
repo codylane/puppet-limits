716d768c0f7f12f6688165828fd2edd15571c6d7
codylane <cody.lane@gmail.com>
2018-04-17 09:39:51 -0600

    more cleanup of before_install.sh

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -xe
 
-update_shell_profile() {
-  echo 'export PATH="${RBENV_ROOT}/bin:$PATH"' >> ${SHELL_PROFILE}
-  echo 'eval "$(rbenv init -)"' >> ${SHELL_PROFILE}
+update_env_profile() {
+  export PATH="${RBENV_ROOT}/bin:$PATH"
+  eval "$(rbenv init -)"
 }
-
-SHELL_PROFILE="${HOME}/.bashrc"
-
-if [ -f "${HOME}/.bashrc" ]; then
-  SHELL_PROFILE="${HOME}/.bashrc"
-elif [ -f "${HOME}/.bash_profile" ]; then
-  SHELL_PROFILE="${HOME}/.bash_profile"
-fi
 
 rm -f Gemfile.lock
 rm -rf .bundle/
@@ -23,12 +15,7 @@ if [ ! -d ${RBENV_ROOT} ]; then
   cd ${RBENV_ROOT} && src/configure && make -C src
 fi
 
-type -P rbenv >>/dev/null 2>&1 || update_shell_profile
-
-# adding the export incase this never gets to the profile.
-export PATH="${RBENV_ROOT}/bin:${PATH}"
-eval "$(rbenv init -)"
-. ${SHELL_PROFILE}
+type -P rbenv >>/dev/null 2>&1 || update_env_profile
 
 mkdir -p ${RBENV_ROOT}/plugins
 [ -d "${RBENV_ROOT}/plugins/ruby-build" ] || git clone https://github.com/rbenv/ruby-build.git ${RBENV_ROOT}/plugins/ruby-build
@@ -36,4 +23,4 @@ mkdir -p ${RBENV_ROOT}/plugins
 rbenv install --skip-existing 2.4.3
 rbenv global 2.4.3
 gem update --system --no-document
-gem install --no-document bundler rake
+gem install --no-document --force bundler rake
