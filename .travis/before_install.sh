e7222c487fb40f8c45ab787704cba32dba9a9108
codylane <cody.lane@gmail.com>
2018-04-17 09:14:08 -0600

    fixes more logic problems in before_install.sh

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,17 +3,32 @@
 rm -f Gemfile.lock
 rm -rf .bundle/
 
-if [ ! -d ~/.rbenv ]; then
-  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-  cd ~/.rbenv && src/configure && make -C src
+export RBENV_ROOT="${HOME}/.rbenv"
 
-  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+SHELL_PROFILE="${HOME}/.bashrc"
+
+if [ ! -d ${RBENV_ROOT} ]; then
+  git clone https://github.com/rbenv/rbenv.git ${RBENV_ROOT}
+  cd ${RBENV_ROOT} && src/configure && make -C src
+
+  if [ -f "${HOME}/.bashrc" ]; then
+    SHELL_PROFILE="${HOME}/.bashrc"
+
+    echo 'export PATH="${RBENV_ROOT}/bin:$PATH"' >> ${SHELL_PROFILE}
+    echo 'eval "$(rbenv init -)"' >> ${SHELL_PROFILE}
+  elif [ -f "${HOME}/.bash_profile" ]; then
+    SHELL_PROFILE="${HOME}/.bash_profile"
+
+    echo 'export PATH="${RBENV_ROOT}/bin:$PATH"' >> ${SHELL_PROFILE}
+    echo 'eval "$(rbenv init -)"' >> ${SHELL_PROFILE}
+  fi
+
 fi
 
-. ~/.bashrc
-mkdir -p "$(rbenv root)"/plugins
-[ -d "$(rbenv root)/plugins/ruby-build" ] || git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
+[ -f ${SHELL_PROFILE} ] && . ${SHELL_PROFILE} || . ${HOME}/.bash_profile
+
+mkdir -p ${RBENV_ROOT}/plugins
+[ -d "${RBENV_ROOT}/plugins/ruby-build" ] || git clone https://github.com/rbenv/ruby-build.git ${RBENV_ROOT}/plugins/ruby-build
 
 rbenv install --skip-existing 2.4.3
 rbenv global 2.4.3
